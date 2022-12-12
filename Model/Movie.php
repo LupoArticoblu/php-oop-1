5bd84aeb72d8e95e55751bd7418b54243adae0c9
LupoArticoblu <wolf94-@libero.it>
2022-12-12 23:58:06 +0100

    esercizio base completo

--- Model/Movie.php
+++ Model/Movie.php
@@ -5,19 +5,20 @@ class Movie{
 public $title;
 public $duration;
 public $poster;
-public $genere;
+public $generi;
 
 /**
  * @var String $title
  * @var String $duration
  * @var String $poster
+ * @var Array $generi
  */
-public function __construct($_title, $_duration, $_poster, genere $_genere)
+public function __construct($_title, $_duration, $_poster, $_generi)
 {
   $this->title = $_title;
   $this->duration = $_duration;
   $this->poster = $_poster;
-  $this->genere = $_genere;
+  $this->genere = $_generi;
 
 }
 
--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 require_once './Model/movie.php';
 require_once './Model/Genere.php';
 
-$film0 = new Movie('Matrix', 120, 'https://www.themoviedb.org/t/p/original/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg', new Genere('Fantascentifico'));
+$genres_film1 = [
+  new Genere('sci-fi'),
+  new Genere('action')
+];
 
-$film1 = new Movie('Starwars', 120, 'https://www.themoviedb.org/t/p/original/nJcAQO0zauDTp72SEMcneIDjA7c.jpg', new Genere('Fantascentifico'));
+$genres_film0 = [
+  new Genere('sci-fi'),
+  new Genere('fantascienza')
+];
+
+$film0 = new Movie('Matrix', 120, 'https://www.themoviedb.org/t/p/original/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg', $genres_film0);
+
+$film1 = new Movie('Starwars', 120, 'https://www.themoviedb.org/t/p/original/nJcAQO0zauDTp72SEMcneIDjA7c.jpg', $genres_film1);
 
 var_dump($film0);
 var_dump($film1);
@@ -13,5 +23,4 @@ var_dump($film1);
 var_dump($film0->get_movie_details());
 var_dump($film1->get_movie_details());
 
-var_dump($film1->genere->name);
 ?>
